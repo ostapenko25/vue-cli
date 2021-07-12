0d8fc6490b9e25e59302e618edd16d07510a1b4e
Yulia Ostapenko <ostapenko25@ukr.net>
2021-07-12 10:36:36 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+
+     <!-- <wrapper-header> -->
+
+    <div class="wrapper-content">    
+
+      <section>
+         <div class="container">
+            <div id="app">
+              <h1>Hello</h1>
+              <img alt="Vue logo" src="./assets/logo.png">
+              <HelloWorld msg="Welcome to Your Vue.js App"/>
+            </div>
+         </div>
+      </section>
+
+    </div>
+
+     <!-- <wrapper-footer> -->
+
+  </div> 
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+
+</style>
